2d4f38301c885a1d913a7034a7f53b774a2b14f2
Pablo Penalva <ppenalva@me.com>
2023-01-20 18:05:05 +0100

    Initial Commit

---
+++ ExpenseSplit/ExpenseSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseSplitApp.swift
+//  ExpenseSplit
+//
+//  Created by Pablo Penalva on 20/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
